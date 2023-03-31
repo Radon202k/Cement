0c37255645577dd1b7f460662e65f22863860a5c
Radon202k <rafael.abreu.coelho@gmail.com>
2023-03-31 06:34:12 -0300

    Create time.c

---
+++ networking/time.c
@@ -0,0 +1,152 @@
+/* Includes for either Windows or Linux */
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
+/* Helpers to make it work on Windows and Linux */
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define GETSOCKETERRNO() (errno)
+#define SOCKET int
+
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main ()
+{
+	SOCKET socket_listen;
+
+	/* Initialize WinSock */
+#if defined(_WIN32)
+	WSADATA d;
+	if (WSAStartup(MAKEWORD(2, 2), &d)) {
+		printf("Failed to initialize.\n");
+		return -1;
+	}
+#endif
+
+	/* Figure out local address the web server will bind to */
+	printf("Configuring local address...\n");
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	struct addrinfo *bind_address;
+	getaddrinfo(0, "8080", &hints, &bind_address);
+
+	/* Create the socket */
+	printf("Creating socket...\n");
+	socket_listen = socket(bind_address->ai_family,
+		bind_address->ai_socktype, bind_address->ai_protocol);
+	if (!ISVALIDSOCKET(socket_listen))
+	{
+		fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+		return 1;
+	}
+
+	/* Bind socket to associate it with our address from getaddrinfo() */
+	printf("Binding socket to local address...\n");
+	if (bind(socket_listen,
+		bind_address->ai_addr, bind_address->ai_addrlen))
+	{
+		fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+		return 1;
+	}
+	freeaddrinfo(bind_address);
+
+	/* Make the socket listen to connections */
+	printf("Listening...\n");
+	if (listen(socket_listen, 10) < 0)
+	{
+		fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+		return 1;
+	}
+
+	/* Accept any incoming connection */
+	printf("Waiting for connection...\n");
+	struct sockaddr_storage client_address;
+	socklen_t client_len = sizeof(client_address);
+	SOCKET socket_client = accept(socket_listen,
+		(struct sockaddr *)&client_address, &client_len);
+	if (!ISVALIDSOCKET(socket_client))
+	{
+		fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
+		return 1;	
+	}
+
+	/* Print the client's address to console */
+	printf("Client is connected... ");
+	char address_buffer[100];
+	getnameinfo((struct sockaddr *)&client_address,
+		client_len, address_buffer, sizeof(address_buffer), 0, 0,
+		NI_NUMERICHOST);
+	printf("%s\n", address_buffer);
+
+	/* Receive a HTTP request */
+	printf("Reading request...\n");
+	char request[1024];
+	int bytes_received = recv(socket_client, request, 1024, 0);
+	printf("Received %d bytes.\n", bytes_received);
+
+	/* Print the browser's request to the console */
+	printf("%.*s", bytes_received, request);
+
+	/* Send our response back */
+	printf("Sending response...\n");
+	const char *response =
+		"HTTP/1.1 200 OK\r\n"
+		"Connection: close\r\n"
+		"Content-Type: text/plain\r\n\r\n"
+		"Local time is: ";
+	int bytes_sent = send(socket_client, response, strlen(response), 0);
+	printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+
+	/* Send the actual time */
+	time_t timer;
+	time(&timer);
+	char *time_msg = ctime(&timer);
+	bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+	printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
+
+	/* Close the client connection to indicate we sent all our data */
+	printf("Closing connection...\n");
+	CLOSESOCKET(socket_client);
+
+	/* Close the listening socket and terminate the program */
+	printf("Closing listening socket...\n");
+	CLOSESOCKET(socket_listen);
+
+#if defined(_WIN32)
+	WSACleanup();
+#endif
+
+	printf("Finished.\n");
+
+	return 0;
+}
